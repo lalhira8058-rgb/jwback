f02ed68367058bc309de110f86825f3793c8d0fa
kapil562 <166809626+kapil562@users.noreply.github.com>
2026-08-25 20:10:00 +0530

    vercel deploy ready - serverless export, vercel.json added

--- server.js
+++ server.js
@@ -13,7 +13,19 @@ const cardRoutes = require("./routes/cards");
 
 const app = express();
 
-connectDB();
+let isConnected = false;
+
+async function ensureDB() {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+}
+
+app.use(async (req, res, next) => {
+  await ensureDB();
+  next();
+});
 
 app.use(cors());
 app.use(express.json());
@@ -30,5 +42,9 @@ app.get("/", (req, res) => {
   res.send("Luxe Gem API Running");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
+
+if (process.env.VERCEL !== "1") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
